b7ca8b0e7e0a30dc29022a94761a3f2f70898167
Wahbigbig <wahbigbig@gmail.com>
2016-10-17 15:18:09 +1100

    init

---
+++ app.js
@@ -0,0 +1,49 @@
+var access_key = process.env.AWS_ALEXA_ACCESS_KEY || '';
+var secret = process.env.AWS_ALEXA_SECRET || '';
+
+var awsalexa = require('./awsalexa.js')(access_key, secret);
+
+/*
+var options = [
+  {key: 'ResponseGroup', value: 'Country'},
+  {key: 'Start', value: 1},
+  {key: 'Count', value: 10},
+  {key: 'CountryCode', value: 'HK'}
+];
+awsalexa.alexatopsite(options, function(err, response) {
+  console.log(response.text);
+});
+*/
+
+/*
+  response group options:
+  http://docs.aws.amazon.com/AlexaWebInfoService/latest/
+*/
+/*
+var options = [
+  {key: 'Url', value: 'http://www.promise.com.hk'},
+  {key: 'ResponseGroup', value: 'Related'}
+];
+awsalexa.alexawis_urlinfo(options, function(err, response) {
+  console.log(response.text);
+});
+*/
+/*
+var options = [
+  {key: 'Url', value: 'http://www.promise.com.hk'},
+  {key: 'ResponseGroup', value: 'History'},
+  {key: 'Start', value: '20161001'}
+];
+awsalexa.alexaawis_traffichistory(options, function(err, response) {
+  console.log(response.text);
+});
+*/
+
+var options = [
+  {key: 'Url', value: 'http://www.promise.com.hk'},
+  {key: 'ResponseGroup', value: 'SitesLinkingIn'},
+  {key: 'Start', value: '0'}
+];
+awsalexa.alexaawis_traffichistory(options, function(err, response) {
+  console.log(response.text);
+});
---
+++ awsalexa.js
@@ -0,0 +1,97 @@
+var crypto = require('crypto');
+var request = require('superagent');
+
+module.exports = function(access_key, secret) {
+
+  function alexatopsite(options, callback) {
+    var endpoint = 'ats.amazonaws.com';
+    options.push({key: 'Action', value: 'TopSites'});
+    options.push({key: 'AWSAccessKeyId', value: access_key});
+    options.push({key: 'Timestamp', value: new Date().toISOString()});
+    options.push({key: 'SignatureVersion', value: 2});
+    options.push({key: 'SignatureMethod', value: 'HmacSHA1'});
+    process(endpoint, options, callback);
+  }
+
+  function alexawis_urlinfo(options, callback) {
+    var endpoint = 'awis.amazonaws.com';
+    options.push({key: 'Action', value: 'UrlInfo'});
+    options.push({key: 'AWSAccessKeyId', value: access_key});
+    options.push({key: 'Timestamp', value: new Date().toISOString()});
+    options.push({key: 'SignatureVersion', value: 2});
+    options.push({key: 'SignatureMethod', value: 'HmacSHA1'});
+    process(endpoint, options, callback);
+  }
+
+  function alexaawis_traffichistory(options, callback) {
+    var endpoint = 'awis.amazonaws.com';
+    options.push({key: 'Action', value: 'TrafficHistory'});
+    options.push({key: 'AWSAccessKeyId', value: access_key});
+    options.push({key: 'Timestamp', value: new Date().toISOString()});
+    options.push({key: 'SignatureVersion', value: 2});
+    options.push({key: 'SignatureMethod', value: 'HmacSHA1'});
+    process(endpoint, options, callback);
+  }
+
+  function alexawis_siteslinkingin(options, callback) {
+    var endpoint = 'awis.amazonaws.com';
+    options.push({key: 'Action', value: 'SitesLinkingIn'});
+    options.push({key: 'AWSAccessKeyId', value: access_key});
+    options.push({key: 'Timestamp', value: new Date().toISOString()});
+    options.push({key: 'SignatureVersion', value: 2});
+    options.push({key: 'SignatureMethod', value: 'HmacSHA1'});
+    process(endpoint, options, callback);
+  }
+
+  function process(endpoint, options, callback) {
+    var encryption = '';
+
+    options.map(function(obj) {
+      if (obj.key == "SignatureMethod") {
+        switch (obj.value) {
+          case 'HmacSHA1':
+            encryption = 'sha1';
+            break;
+            case'HmacSHA256':
+            encryption ='sha256'
+            break;
+          default:
+            encryption = 'sha1';
+            obj.value = 'HmacSHA1';
+        }
+      }
+    });
+
+    //sort params in alphabet order
+    var params = options.sort(function(a, b) {
+      var keyA = a.key, keyB = b.key;
+      if (keyA < keyB) {
+        return -1;
+      }
+      if (keyB < keyA) {
+        return 1;
+      }
+      return 0;
+    });
+
+    //convert params to query
+    var query = params.map(function(obj) {
+      return obj.key + "=" + encodeURIComponent(obj.value);
+    }).join('&');
+
+    var sign_str = "GET\n" + endpoint + "\n/\n" + query;
+    //encrypt with SHA1 or SHA256 according to the options
+    var signature = crypto.createHmac(encryption, secret).update(sign_str).digest('base64');
+    var url = "http://" + endpoint + "/?" + (query) + "&Signature=" + signature;
+
+    request.get(url).end(function(err, res) {
+      callback(err, res);
+    });
+  }
+
+  return {
+    alexatopsite: alexatopsite,
+    alexawis_urlinfo: alexawis_urlinfo,
+    alexaawis_traffichistory: alexaawis_traffichistory
+  }
+}
